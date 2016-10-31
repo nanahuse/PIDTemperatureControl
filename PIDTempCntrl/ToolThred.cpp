3ca7bfa0f1c1e1fb3a95ddbe17ca3975bdce770f
Nanahuse <sznx_com@hotmail.co.jp>
2016-10-31 20:18:08 +0900

    BuzzerとButtonのクラスを別の場所に 機能的な書き換えはない．

---
+++ PIDTempCntrl/ToolThred.cpp
@@ -0,0 +1,138 @@
+﻿// 
+// 
+// 
+
+#include "ToolThred.h"
+
+BuzzerThread::BuzzerThread(uint8_t controlPin)
+{
+	ControlPin = controlPin;
+	pinMode(ControlPin, OUTPUT);
+	digitalWrite(ControlPin, LOW);
+	this->Stop();
+}
+
+void BuzzerThread::SetSound(unsigned long interval, unsigned long soundLength)
+{
+	Interval = interval;
+	Length = soundLength;
+}
+
+void BuzzerThread::SoundOnce(unsigned long soundLength)
+{
+	SetTimerStop(IntervalTimer);
+	LengthTimer = millis() + soundLength;
+	SetTimerStop(Interval);
+	SetTimerStop(Length);
+	digitalWrite(ControlPin, HIGH);
+}
+
+bool BuzzerThread::Tick()
+{
+	if ( millis() >= IntervalTimer )
+	{
+		digitalWrite(ControlPin, HIGH);
+		UpdateIntervalTImer();
+		return true;
+	}
+	if ( millis() >= LengthTimer )
+	{
+		digitalWrite(ControlPin, LOW);
+		LengthTimer = IntervalTimer + Length;
+	}
+	return false;
+}
+
+void BuzzerThread::Start()
+{
+	IntervalTimer = millis() + Interval;
+	LengthTimer = IntervalTimer + Length;
+}
+
+void BuzzerThread::Stop()
+{
+	digitalWrite(ControlPin, LOW);
+	SetTimerStop(IntervalTimer);
+	SetTimerStop(LengthTimer);
+}
+
+void BuzzerThread::UpdateIntervalTImer()
+{
+	unsigned long NowTime = millis();
+	while ( IntervalTimer < NowTime )
+	{
+		IntervalTimer += Interval;
+	}
+}
+
+ButtonClass::ButtonClass(uint8_t controlPin, bool canRepeat) : Timer()
+{
+	ControlPin = controlPin;
+	pinMode(ControlPin, INPUT_PULLUP);
+	SetCanRepeat(CanRepeat);
+	SetRepeatTime(100);
+	OldStatus = false;
+}
+
+void ButtonClass::SetRepeatTime(unsigned long RepeatTime)
+{
+	if ( CanRepeat )
+	{
+		Timer.SetInterval(RepeatTime);
+	}
+}
+
+void ButtonClass::SetCanRepeat(bool canRepeat)
+{
+	CanRepeat = canRepeat;
+	if ( !CanRepeat )
+	{
+		Timer.SetInterval(100); //チャタリングの防止用
+		OldStatus = false;
+	}
+}
+
+bool ButtonClass::isPressed()
+{
+	if ( !digitalRead(ControlPin) ) //プルアップ回路なのでdigitalReadがfalseの時に押されている。
+	{
+		if ( Timer.Tick() )
+		{
+			if ( OldStatus )
+			{
+				if ( CanRepeat )
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+					Timer.Stop();
+				}
+			}
+			else
+			{
+				Timer.Start();
+				OldStatus = true;
+				return true;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		if ( OldStatus )
+		{
+			OldStatus = false;
+			if ( !CanRepeat )
+			{
+				Timer.Start();
+			}
+		}
+		return false;
+
+	}
+}
---
+++ PIDTempCntrl/ToolThred.h
@@ -0,0 +1,57 @@
+﻿// ToolThred.h
+
+#ifndef _TOOLTHRED_h
+#define _TOOLTHRED_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+#include "ThreadBase.h"
+
+/*
+擬似並列実行可能なブザー
+一度だけ鳴らす、一定周期ごとに繰り返し鳴らすのどちらかが可能。
+*/
+class BuzzerThread :private SimpleTimerThread
+{
+public:
+	BuzzerThread(uint8_t controlPin);
+	void SetSound(unsigned long interval, unsigned long soundLength); //intervalごとにsoundLengthの長さブザーを鳴らす。
+	void SoundOnce(unsigned long soundLength); //実行時からSoundLengthの長さブザーを鳴らす。一度鳴るとタイマーが止まる。
+	bool Tick(); //音が鳴り始める時にTrueを返す。
+	void Start();
+	void Stop();
+
+private:
+	unsigned long LengthTimer;
+	unsigned long Interval, Length;
+	uint8_t ControlPin;
+	void UpdateIntervalTImer();
+};
+
+
+/*
+INPUT_PULLUPを使ったボタンクラス。タクトスイッチの接続に注意。
+CanRepeatをFalseにすると一度だけ
+CanRepeatをTrueにすると押しっぱなしの時はRepeatTimeの間隔でTrueを返す。
+ループしながらボタン入力をするときを想定。
+*/
+class ButtonClass
+{
+public:
+	ButtonClass(uint8_t controlPin, bool canRepeat);
+	void SetRepeatTime(unsigned long RepeatTime);
+	void SetCanRepeat(bool canRepeat);
+	bool isPressed();
+private:
+	SimpleTimerThread Timer;
+	uint8_t ControlPin;
+	bool OldStatus;
+	bool CanRepeat;
+};
+
+#endif
+
